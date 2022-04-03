21e8e41dc0043ddd9ddb1459fd5ad1cbb1ff2978
Ismail Hossain <angred.ismail@gmail.com>
2022-04-03 18:15:53 +0800

    bug fix and added branch profile in drawer

---
+++ cashier/lib/screens/branch_screens/branch_profile.dart
@@ -0,0 +1,14 @@
+// import 'package:flutter/material.dart';
+
+// class BranchProfile extends StatelessWidget {
+//   const BranchProfile({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Branch Profile'),
+//       ),
+//     );
+//   }
+// }
